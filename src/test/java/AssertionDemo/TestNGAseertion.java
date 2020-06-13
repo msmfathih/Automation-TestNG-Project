6038ecbb2f61d65b6698df4c261c0721d7069c3c
msmfathih <msmfathih@users.noreply.github.com>
2020-06-13 06:42:42 +0400

    TestNG Assert Demo

---
+++ src/test/java/AssertionDemo/TestNGAseertion.java
@@ -0,0 +1,32 @@
+package AssertionDemo;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import junit.framework.Assert;
+
+public class TestNGAseertion {
+
+	
+	@Test
+	public void softAssertionTest() {
+		
+		SoftAssert assertion=new SoftAssert();
+		System.out.println("Test 1 Started");
+		assertion.assertEquals(12, 13);
+		System.out.println("Test 1 Completed");
+		assertion.assertAll(); //soft assert method call function
+	}
+	
+	
+	@Test
+	public void hardAssertionTest() {
+		
+		System.out.println("Test 2 Started");
+		Assert.assertEquals(12, 13);
+		System.out.println("Test 2 Completed");
+		
+	}
+	
+	
+}
